afe8352a58cf0bdf297616b72d78b48f10244987
malol01 <121419024+malol01@users.noreply.github.com>
2024-07-22 14:03:41 -0700

    Add files via upload

---
+++ download_and_extract.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Array of URLs to download
+urls=(
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-armv4l.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-armv5l.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-armv6l.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-armv7l.tar.bz2"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-i586.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-m68k.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-mips.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-mipsel.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-powerpc.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-sh4.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-sparc.tar.gz"
+    "https://raw.githubusercontent.com/malol01/cross-compiler-for-mirai-archive/main/cross-compiler-x86_64.tar.bz2"
+)
+
+# Download and extract each file
+for url in "${urls[@]}"; do
+    filename=$(basename "$url")
+    curl -O "$url"
+    
+    # Determine the file type and extract accordingly
+    if [[ "$filename" == *.tar.gz ]]; then
+        tar -xzf "$filename"
+    elif [[ "$filename" == *.tar.bz2 ]]; then
+        tar -xjf "$filename"
+    else
+        echo "Unknown file type for $filename"
+    fi
+done
+
