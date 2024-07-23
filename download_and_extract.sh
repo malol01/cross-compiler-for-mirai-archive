09c536786f0cb775231ec73c79e8bd20996432e4
malol01 <121419024+malol01@users.noreply.github.com>
2024-07-22 17:19:47 -0700

    Update download_and_extract.sh

--- download_and_extract.sh
+++ download_and_extract.sh
@@ -24,14 +24,27 @@ for url in "${urls[@]}"; do
     # Determine the file type and extract accordingly
     if [[ "$filename" == *.tar.gz ]]; then
         tar -xzf "$filename"
-        rm "$filename"  # Remove the archive file after extraction
     elif [[ "$filename" == *.tar.bz2 ]]; then
         tar -xjf "$filename"
-        rm "$filename"  # Remove the archive file after extraction
     else
         echo "Unknown file type for $filename"
     fi
+    rm "$filename"  # Remove the archive file after extraction
 done
+
+# Move extracted directories to new names
+mv cross-compiler-i586 i586
+mv cross-compiler-m68k m68k
+mv cross-compiler-mips mips
+mv cross-compiler-mipsel mipsel
+mv cross-compiler-powerpc powerpc
+mv cross-compiler-sh4 sh4
+mv cross-compiler-sparc sparc
+mv cross-compiler-armv4l armv4l
+mv cross-compiler-armv5l armv5l
+mv cross-compiler-armv6l armv6l
+mv cross-compiler-armv7l armv7l
+mv cross-compiler-x86_64 x86_64
 
 # Display final message
 echo "thanks to my creator malol01 :)"
